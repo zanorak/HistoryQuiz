4f461c57a61a2c7efa64c16a975cbcd4c48c49a6
James Walsh <zanorakdaggorath@gmail.com>
2022-02-27 16:47:58 -0800

    Finish writing quiz

--- quiz.js
+++ quiz.js
@@ -1,3 +1,22 @@
 $(document).ready(function(){
-  $("#1").flip();
+  $('.card').hide();
+  $('#cards>div').eq(0).show();
+  $(".card").flip();
+
+  // https://stackoverflow.com/questions/28535226/javascript-how-to-show-the-next-div-and-hide-the-previous-one
+  $('#next').on('click', function() {
+    $('#cards>div').each(function() {
+      var id = $(this).index();
+      if ($(this).is(':visible')) {
+        $(this).hide();
+        if (id == $('#cards>div').length - 1) {
+          $('#cards>div').eq(0).show();
+        } else {
+          $('#cards>div').eq(id + 1).show();
+        }
+        return false;
+      }
+    });
+  });
+
 });
